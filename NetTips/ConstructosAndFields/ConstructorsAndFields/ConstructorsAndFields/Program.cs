69352ceb14f53ea1a2fb007a55b51817f2ad820e
deadean <deadean@yandex.ru>
2015-06-24 20:22:24 +0300

    #update

---
+++ NetTips/ConstructosAndFields/ConstructorsAndFields/ConstructorsAndFields/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndFields
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A a = new A();
+		}
+	}
+
+	class A
+	{
+		protected int _field;
+
+		public A()
+		{
+			_field = 9;
+		}
+
+		public A(int b):this()
+		{
+
+		}
+	}
+
+	class B : A
+	{
+		public B()
+		{
+			_field = 10;
+		}
+	}
+}
