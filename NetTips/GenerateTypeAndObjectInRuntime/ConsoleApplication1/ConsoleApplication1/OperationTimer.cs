3c2ee55c5584fb305cc184e6b10845753778288b
deadean <deadean@yandex.ru>
2015-07-13 20:21:05 +0300

    #added GenerateTypeAndObjectInRuntime

---
+++ NetTips/GenerateTypeAndObjectInRuntime/ConsoleApplication1/ConsoleApplication1/OperationTimer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	public sealed class OperationTimer:IDisposable
+	{
+		private System.Diagnostics.Stopwatch _start;
+		private String _text;
+		private Int32 _collectionCount;
+
+		public OperationTimer(String text)
+		{
+			PrepareForOperation();
+
+			_text = text;
+			_collectionCount = GC.CollectionCount(0);
+
+			_start = System.Diagnostics.Stopwatch.StartNew();
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("{0} (GCs={1},{2}) {3}", _start.Elapsed, GC.CollectionCount(0), _collectionCount, _text);
+		}
+
+		private static void PrepareForOperation()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+		}
+	}
+}
