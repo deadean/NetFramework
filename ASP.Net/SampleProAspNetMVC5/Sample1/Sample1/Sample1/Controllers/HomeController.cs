b3154c5caa5babe5a7bf1979fcc07621d11b1118
deadean <deadean@yandex.ru>
2015-08-12 11:21:53 +0300

    #Created AspNetMvc5Sample directory

---
+++ ASP.Net/SampleProAspNetMVC5/Sample1/Sample1/Sample1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample1.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ViewResult Index()
+		{
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+			return View();
+		}
+
+		//public ActionResult Index()
+		//{
+		//	return View();
+		//}
+
+		//public string Index()
+		//{
+		//	return "Hello world";
+		//}
+	}
+}
