4abe94f7cf74047f49fd0599d129089c53918499
deadean <deadean@yandex.ru>
2015-08-17 17:09:27 +0300

    #Update AspMvc Sample

--- ASP.Net/SampleProAspNetMVC5/Sample1/Sample1/Sample1/Controllers/HomeController.cs
+++ ASP.Net/SampleProAspNetMVC5/Sample1/Sample1/Sample1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sample1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,18 @@ namespace Sample1.Controllers
 			int hour = DateTime.Now.Hour;
 			ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
 			return View();
+		}
+
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse response)
+		{
+			return View("Thanks", response);
 		}
 
 
