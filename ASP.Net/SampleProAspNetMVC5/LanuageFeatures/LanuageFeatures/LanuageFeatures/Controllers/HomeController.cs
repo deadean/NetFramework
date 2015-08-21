30c8ac30de902e794a49b2f03356b039533d7d6e
deadean <deadean@yandex.ru>
2015-08-21 16:26:25 +0300

    #Finished Ch5

--- ASP.Net/SampleProAspNetMVC5/LanuageFeatures/LanuageFeatures/LanuageFeatures/Controllers/HomeController.cs
+++ ASP.Net/SampleProAspNetMVC5/LanuageFeatures/LanuageFeatures/LanuageFeatures/Controllers/HomeController.cs
@@ -24,6 +24,34 @@ namespace LanuageFeatures.Controllers
 			return View(modProduct);
 		}
 
+		public ActionResult NameAndPrice()
+		{
+			return View(modProduct);
+		}
+
+		public ActionResult DemoExpression()
+		{
+			ViewBag.ProductCount = 1;
+			ViewBag.ExpressShip = true;
+			ViewBag.ApplyDiscount = false;
+			ViewBag.Supplier = null;
+
+			return View(modProduct);
+		}
+
+		public ActionResult DemoArray()
+		{
+			Product[] array =
+			{
+				new Product { Name="Kayak", Price=275M},
+				new Product { Name="Lifejacket", Price=48.95M},
+				new Product { Name="Soccer ball", Price=19.50M},
+				new Product { Name="Corner flag", Price=34.95M}
+			};
+
+			return View(array);
+		}
+
 		public ViewResult AutoProperty()
 		{
 			Product product = new Product();
