f097dbe18e5af24815eccf155bb9abd0ccc35479
deadean <deadean@yandex.ru>
2015-08-23 17:22:11 +0300

    #Started Ch7

---
+++ ASP.Net/SampleProAspNetMVC5/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		private IKernel modKernel;
+
+		public NinjectDependencyResolver(IKernel kernel)
+		{
+			modKernel = kernel;
+			AddBindings();
+		}
+
+		private void AddBindings()
+		{
+		}
+
+		public object GetService(Type serviceType)
+		{
+			return modKernel.TryGet(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return modKernel.GetAll(serviceType);
+		}
+	}
+}
