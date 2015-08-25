a0f85bd346018845bc88f11968a1df5d6f7cf906
deadean <deadean@yandex.ru>
2015-08-25 21:03:37 +0300

    #Working ch6

--- ASP.Net/SampleProAspNetMVC5/SportsStore/SportsStore.Tests/UnitTest1.cs
+++ ASP.Net/SampleProAspNetMVC5/SportsStore/SportsStore.Tests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
 
 namespace SportsStore.Tests
 {
@@ -9,6 +15,27 @@ namespace SportsStore.Tests
 		[TestMethod]
 		public void TestMethod1()
 		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(
+				new Product[]
+				{
+					new Product { ProductID=1, Name="Product1"},
+					new Product { ProductID=2, Name="Product2"},
+					new Product { ProductID=3, Name="Product3"},
+					new Product { ProductID=4, Name="Product4"},
+					new Product { ProductID=5, Name="Product5"}
+				}
+			);
+
+			ProductController controller = new ProductController(mock.Object);
+			controller.PageSize = 3;
+
+			IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+			Product[] prodArray = result.ToArray();
+
+			Assert.IsTrue(prodArray.Length == 2);
+			Assert.AreEqual(prodArray[0].Name, "Product4");
+			Assert.AreEqual(prodArray[1].Name, "Product5");
 		}
 	}
 }
--- ASP.Net/SampleProAspNetMVC5/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ ASP.Net/SampleProAspNetMVC5/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -9,11 +9,13 @@ namespace SportsStore.WebUI.Controllers
 {
 	public class ProductController : Controller
 	{
+		
 		private IProductRepository modProductRepository;
 
 		public ProductController(IProductRepository productRepo)
 		{
 			modProductRepository = productRepo;
+			PageSize = 4;
 		}
 
 		public ActionResult Index()
@@ -21,10 +23,12 @@ namespace SportsStore.WebUI.Controllers
 			return View();
 		}
 
-		public ViewResult List()
+		public ViewResult List(int page = 1)
 		{
 			var products = modProductRepository.Products.ToList();
-      return View(products);
+      return View(products.OrderBy(x=>x.ProductID).Skip((page-1)* PageSize).Take(PageSize));
 		}
+
+		public int PageSize { get; set; }
 	}
 }
